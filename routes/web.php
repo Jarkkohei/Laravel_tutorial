bb9445418fce3c8030f13e9cccc2fc91e5f42788
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-06 16:49:26 +0200

    Added the about- and services-views and the PagesController and routes for them. Situation after the course Part 3

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+/*  Routing Examples
+Route::get('/hello', function () {
+    return '<h1>Hello World!</h1>';
 });
+
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return 'This is user ' .$name.' with an id of '.$id;
+});
+*/
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
+
+
 
