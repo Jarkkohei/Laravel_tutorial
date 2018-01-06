387b002c7ae1069b6823dd4e11c3f6226a89dada
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-06 17:57:23 +0200

    Blade templating and passing data to views

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,15 +7,25 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index() {
-        return view('pages.index');
+        $title = 'Welcome to Laravel!';
+        //return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
     }
 
     public function about() {
-        return view('pages.about');
+        $title = 'About us';
+        return view('pages.about')->with('title', $title);
+
     }
 
     public function services() {
-        return view('pages.services');
+
+        $data = array(
+            'title' => 'Our services',
+            'services' => ['Web desing', 'Programing', 'SEO']
+        );
+
+        return view('pages.services')->with($data);
     }
 
 
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,15 +1,12 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-
-    </head>
-    <body>
-        <h1>Services</h1>
-        <p>This is the services page.</p>
-    </body>
-</html>
+@section('content')
+    <h1>{{$title}}</h1>
+    @if(count($services) > 0)
+        <ul>
+        @foreach($services as $service)
+            <li>{{$service}}</li>
+        @endforeach
+        </ul>
+    @endif
+@endsection
