4cab09c3cef0b7a5211e7f77be400ae5f0143052
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-09 19:27:36 +0200

    Hid the "edit"- and "delete"-buttons from authenticated non-authors.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,12 +14,14 @@
     <hr>
 
     @if(!Auth::guest())
-        <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+        @if(Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 
-        {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
-            {{Form::hidden('_method', 'DELETE')}}
-            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-        {!! Form::close() !!}
+            {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+                {{Form::hidden('_method', 'DELETE')}}
+                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!! Form::close() !!}
+        @endif
     @endif
-    
+
 @endsection
