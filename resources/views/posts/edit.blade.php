0f79ba6a822432ffbf03ce19719bbd51ac6adfe1
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-09 22:22:08 +0200

    Aded the image adding functionality also to the "posts.edit"-view

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Edit Post</h1>
 
-    {!! Form::open(['action' => ['PostsController@update', $post->id], 'method' => 'POST']) !!}
+    {!! Form::open(['action' => ['PostsController@update', $post->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
         <div class="form-group">
             {{Form::label('title', 'Title')}}
             {{Form::text('title', $post->title, ['class' => 'form-control', 'placeholder' => 'Title'])}}
@@ -13,6 +13,10 @@
             {{Form::label('body', 'Body')}}
             {{Form::textarea('body', $post->body, ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
 
+        </div>
+
+        <div class="form-group">
+            {{Form::file('cover_image')}}
         </div>
 
         {{Form::hidden('_method', 'PUT')}}
